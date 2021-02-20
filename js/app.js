5d3c82bb6b4a277e04f9fcff70765ae93be2c0fa
Ingrid-T <tompouceingrid@yahoo.fr>
2021-02-20 12:28:21 +0100

    portfolio v1

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(this).click(function () {
+	if ('#accueil'){}
+		else if ('#qui') {}
+			else if ('#competences') {}
+				else if ('#contact') {}
+})
+$('#qui').click()
+$('#competences').click()
+$('#contact').click()
